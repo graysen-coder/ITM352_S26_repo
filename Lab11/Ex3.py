1ccd7a124970e5c4415bccff14ef7222e24591d6
Graysen Oumi <goumi@hawaii.edu>
2026-03-31 14:05:46 -1000

    class 3/31

--- Lab11/Ex3.py
+++ Lab11/Ex3.py
@@ -7,6 +7,7 @@ import numpy as np
 filename = "https://drive.google.com/uc?id=1ujY0WCcePdotG2xdbLyeECFW9lCJ4t-K"
 
 pd.set_option('display.max_columns', None)  # Show all columns in the output
+pd.set_option('display.float_format', lambda x: f'{x:.2f}')  # Format floats to 2 decimal places
 
 df = pd.read_csv(filename)
 df["order_date"] = pd.to_datetime(df["order_date"], format="%d-%m-%Y", errors='coerce')
@@ -18,8 +19,8 @@ df["sales"] = df["quantity"] * df["unit_price"]
 pivot_table = pd.pivot_table(df,
                              index="sales_region",
                              values="sales",
-                             columns="order_type",
-                             aggfunc=np.sum,
+                             columns=["order_type", state_col],
+                             aggfunc=[np.sum, np.mean],
                              margins=True,
                              margins_name="Total Sales")
 
